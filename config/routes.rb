874633dd7e3d259894cefa81321a28b683858346
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2018-02-13 15:58:33 +0100

    my app is ok without style

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ #read all
+ get 'tasks', to: 'tasks#index', as: :index
+ #CRUD
+ #C
+ get 'tasks/new', to: 'tasks#new', as: :new
+ post 'tasks', to: 'tasks#create'
+ #R
+
+ #read one
+ get 'tasks/:id', to: 'tasks#show', as: :task
+ #U
+ get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+ patch 'tasks/:id', to: 'tasks#update'
+ #D
+ delete 'tasks/:id', to: 'tasks#destroy', as: :destroy
 end
